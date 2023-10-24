69bda508098710dfec54b152d5e63e7188d63182
Ahmad Fajar Kusumajati <111202012995@mhs.dinus.ac.id>
2023-10-24 14:19:21 +0700

    changed prediction.py and prediction_service.py

--- app/api/v1/endpoints/prediction.py
+++ app/api/v1/endpoints/prediction.py
@@ -13,10 +13,17 @@ router = APIRouter()
 service = ForecastService()
 
 
-@router.get("/forecast/id={id}")
-async def get_forecast_id(id: int = Path(..., title="ID of Kelurahan")):
-    # Call the service to get the forecast by 'id'
-    recharts_data = service.get_forecast_by_id(id)
-    if recharts_data is None:
+@router.get("/real-data/id={id}")
+async def get_real_data_by_id(id: int = Path(..., title="ID of Kelurahan")):
+    real_data = service.get_real_data_by_id(id)
+    if not real_data:
         raise HTTPException(status_code=404, detail="ID not found")
-    return recharts_data
+    return real_data
+
+
+@router.get("/predicted-data/id={id}")
+async def get_predicted_data_by_id(id: int = Path(..., title="ID of Kelurahan")):
+    predicted_data = service.get_predicted_data_by_id(id)
+    if not predicted_data:
+        raise HTTPException(status_code=404, detail="ID not found")
+    return predicted_data
--- app/service/prediction_service.py
+++ app/service/prediction_service.py
@@ -1,6 +1,5 @@
 # service.py
 import pandas as pd
-import numpy as np
 from statsmodels.tsa.statespace.sarimax import SARIMAX
 
 
@@ -11,7 +10,36 @@ class ForecastService:
         self.mapping_df = pd.read_csv(
             'app/data/kelurahan_monthly.csv')  # Load the mapping data
 
-    def get_forecast_by_id(self, id: int):
+    def get_real_data_by_id(self, id: int):
+        # Find 'nama_kelurahan' based on 'id' from the mapping data
+        nama_kelurahan = self.mapping_df.loc[self.mapping_df['id']
+                                             == id, 'nama_kelurahan'].values[0]
+
+        # Extract the data for the specified 'nama_kelurahan'
+        data = self.df[self.df['Kelurahan Pasien'] == nama_kelurahan]
+
+        # Remove the "Kelurahan Pasien" column
+        data = data.drop(columns='Kelurahan Pasien')
+
+        # Transpose the DataFrame
+        data = data.transpose()
+        data.columns = ['Value']
+
+        # Reset the index and format the 'Month' column as datetime
+        data.reset_index(inplace=True)
+        data.rename(columns={'index': 'Month'}, inplace=True)
+        data['Month'] = pd.to_datetime(data['Month'], format='%Y-%m')
+
+        # Set the 'Month' column as the index
+        data.set_index('Month', inplace=True)
+
+        # Convert real data to a list of dictionaries for Recharts format
+        real_data = [{"Month": str(date), "RealValue": real_value}
+                     for date, real_value in zip(data.index, data['Value'])]
+
+        return real_data
+
+    def get_predicted_data_by_id(self, id: int):
         # Find 'nama_kelurahan' based on 'id' from the mapping data
         nama_kelurahan = self.mapping_df.loc[self.mapping_df['id']
                                              == id, 'nama_kelurahan'].values[0]
@@ -51,11 +79,8 @@ class ForecastService:
         # Predict values for the future months
         future_predictions = model_fit.get_forecast(steps=len(future_months))
 
-        # Extract the real data
-        real_data = data.copy()
+        # Convert predicted data to a list of dictionaries for Recharts format
+        predicted_data = [{"Month": str(date), "PredictedValue": predicted_value}
+                          for date, predicted_value in zip(future_months, future_predictions.predicted_mean)]
 
-        # Create a list of dictionaries for Recharts format with real and predicted values
-        recharts_data = [{"Month": str(date), "RealValue": real_value, "PredictedValue": predicted_value}
-                         for date, real_value, predicted_value in zip(future_months, real_data['Value'], future_predictions.predicted_mean)]
-
-        return recharts_data
+        return predicted_data
