26cd3d916d2b804ca20e461247c7b62fee19630f
Ahmad Fajar Kusumajati <111202012995@mhs.dinus.ac.id>
2023-10-29 23:44:26 +0700

    changed intervensi.csv and intervention_service.py

--- app/service/intervention_service.py
+++ app/service/intervention_service.py
@@ -25,7 +25,9 @@ class IntervensiService:
         level = level_mapping.get(kategori_kerentanan)
 
         filtered_intervensi = self.intervensi_df[self.intervensi_df['level_intervensi'] == level]
-        isi_intervensi = filtered_intervensi['isi_intervensi'].tolist()
+        # Return a list of dictionaries including both 'judul_intervensi' and 'isi_intervensi'
+        result = [{'judul_intervensi': row['judul_intervensi'], 'isi_intervensi': row['isi_intervensi']}
+                  for _, row in filtered_intervensi.iterrows()]
 
         # Return a list of dictionaries
-        return [{"intervensi": item} for item in isi_intervensi]
+        return result
