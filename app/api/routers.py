080fb6802e315470fa9f8ddd2af44bd0863d03a2
Ahmad Fajar Kusumajati <fajarku26@gmail.com>
2024-04-21 13:00:31 +0700

    changed api prefix

--- app/api/routers.py
+++ app/api/routers.py
@@ -12,8 +12,7 @@ api_router = APIRouter()
 api_router.include_router(
     intervention.router, prefix="/interventions", tags=["interventions"])
 api_router.include_router(survey.router, prefix="/surveys", tags=["surveys"])
-api_router.include_router(cases.router, prefix="/cases", tags=["cases"])
+api_router.include_router(cases.router, tags=["cases"])
 api_router.include_router(
-    prediction.router, prefix="/predictions", tags=["predictions"])
-api_router.include_router(vulnerability.router,
-                          prefix="/vulnerabilities", tags=["vulnerabilities"])
+    prediction.router, tags=["predictions"])
+api_router.include_router(vulnerability.router, tags=["vulnerabilities"])
