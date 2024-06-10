52110c2b9cc98c4761fe3648ca81c498727d5836
Ahmad Fajar Kusumajati <fajarku26@gmail.com>
2024-06-10 13:22:41 +0700

    edit data model

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,5 @@
 from .intervention import Intervention
-from .survey import Survey
+# from .survey import Survey
 from .kelurahan import Kelurahan
 from .patient import Patient
 from .semar_betul_auth import SemarBetulAuth
